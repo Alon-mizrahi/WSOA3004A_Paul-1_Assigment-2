bef365ad493b611833df47d227d3a6ea1c23fabd
RobertJohannsen <2281696@students.wits.ac.za>
2021-09-03 18:29:44 +0200

    cat anim part 2
    -added pickup (reg + fat)
    -added pick up start (reg + fat)
    -added cameraScroll (camDolly script , put it on an empty object and parent the camera to it)

---
+++ Assingment_2/Assets/Rob/camScroll.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camScroll : MonoBehaviour
+{
+    public GameObject camDolly;
+    public float thres , clampX , clampY;
+    public int camMoveSpeed;
+    private float screenThres;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenThres = Screen.width / thres;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float adjustedCamMoveSpeedx = camMoveSpeed * (1 - (Mathf.Clamp(Input.mousePosition.x - Screen.width, 0 , camMoveSpeed)/camMoveSpeed)); // if inside the threshold , camera will move faster the closer it is the edge 
+        float adjustedCamMoveSpeedy = camMoveSpeed * (1 - (Mathf.Clamp(Input.mousePosition.y - Screen.height, 0, camMoveSpeed) / camMoveSpeed));
+      
+
+        if (Input.mousePosition.x < screenThres) 
+            camDolly.transform.position = new Vector3(camDolly.transform.position.x - adjustedCamMoveSpeedx, camDolly.transform.position.y , camDolly.transform.position.z);
+        if (Input.mousePosition.x > Screen.width - screenThres)
+            camDolly.transform.position = new Vector3(camDolly.transform.position.x + adjustedCamMoveSpeedx, camDolly.transform.position.y, camDolly.transform.position.z);
+
+        if (Input.mousePosition.y < screenThres)
+            camDolly.transform.position = new Vector3(camDolly.transform.position.x, camDolly.transform.position.y - adjustedCamMoveSpeedy, camDolly.transform.position.z);
+
+        if (Input.mousePosition.y > Screen.height - screenThres)
+            camDolly.transform.position = new Vector3(camDolly.transform.position.x, camDolly.transform.position.y + adjustedCamMoveSpeedy, camDolly.transform.position.z);
+
+
+        camDolly.transform.position = new Vector3(Mathf.Clamp(camDolly.transform.position.x , -clampX , clampX ), Mathf.Clamp(camDolly.transform.position.y, -clampY, clampY), camDolly.transform.position.z);
+
+    }
+}
