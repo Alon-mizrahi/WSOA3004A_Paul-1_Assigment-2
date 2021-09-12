dd08c440734ddcb1686057bf51b8e16ebe04106a
JulietteLove <80416448+JulietteLove@users.noreply.github.com>
2021-09-12 15:32:48 +0200

    Credits added

--- Assingment_2/Assets/Scenes/ButtonScript.cs
+++ Assingment_2/Assets/Scenes/ButtonScript.cs
@@ -6,9 +6,21 @@ using UnityEngine.SceneManagement;
 public class ButtonScript : MonoBehaviour
 {
     public GameObject credits;
+    public GameObject howToPlay;
+
     public void Play()
     {
         SceneManager.LoadScene("A_scene");
+    }
+
+    public void HowToPlayOpen()
+    {
+        howToPlay.SetActive(true);
+    }
+
+    public void HowToPlayClose()
+    {
+        howToPlay.SetActive(false);
     }
 
     public void CreditsOpen()
